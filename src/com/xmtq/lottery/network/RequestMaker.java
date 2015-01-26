0cde2c18a6b4e5711c381cba13914b0e7c650339
scorpio374 <yvhynjw@gmail.com>
2015-01-26 15:21:44 +0800

    01261521

--- src/com/xmtq/lottery/network/RequestMaker.java
+++ src/com/xmtq/lottery/network/RequestMaker.java
@@ -371,7 +371,12 @@ public class RequestMaker {
 	}
 
 	/**
-	 * 提取现金
+	 * 3.1.10提取现金（20003_1.1）
+	 * 
+	 * @param uid
+	 * @param password
+	 * @param drawalmoney
+	 * @return
 	 */
 	public Request getExtractCash(String uid, String password,
 			String drawalmoney) {
@@ -405,14 +410,30 @@ public class RequestMaker {
 	}
 
 	/**
-	 * 4.投注业务
+	 * 4.1.1投注（12006_1.1）
+	 * 
+	 * @param uid
+	 * @param lotteryid
+	 * @param votetype
+	 * @param votenums
+	 * @param multiple
+	 * @param voteinfo
+	 * @param totalmoney
+	 * @param playtype
+	 * @param passtype
+	 * @param buymoney
+	 * @param protype
+	 * @return
 	 */
+	public Request getBettingBusiness(String uid, String lotteryid,
+			String votetype, String votenums, String multiple, String voteinfo,
+			String totalmoney, String playtype, String passtype,
+			String buymoney, String protype) {
 
-	public Request getBettingBusiness(String uid, String password,
-			String drawalmoney) {
-
-		String body = createExtractCash(uid, password, drawalmoney);
-		String xmlBody = makeXml(body, "20003_1.1");
+		String body = createBettingBusiness(uid, lotteryid, votetype, votenums,
+				multiple, voteinfo, totalmoney, playtype, passtype, buymoney,
+				protype);
+		String xmlBody = makeXml(body, "12006_1.1");
 		LogUtil.log("xmlBody:" + xmlBody);
 
 		request.setBody(xmlBody);
@@ -421,16 +442,113 @@ public class RequestMaker {
 		return request;
 	}
 
-	private String createExtractCash(String uid, String password,
-			String drawalmoney) {
+	private String createBettingBusiness(String uid, String lotteryid,
+			String votetype, String votenums, String multiple, String voteinfo,
+			String totalmoney, String playtype, String passtype,
+			String buymoney, String protype) {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append("<body>");
 		sb.append("<elements>");
-		sb.append("<element drawaltype=\"0\">");
-		sb.append(makeTag("uid", "14244"));
-		sb.append(makeTag("password", "tq111111"));
-		sb.append(makeTag("drawalmoney", "10"));
+		sb.append("<element>");
+		sb.append(makeTag("lotteryid", lotteryid));
+		sb.append(makeTag("uid", uid));
+		sb.append(makeTag("votetype", votetype));
+		sb.append(makeTag("votenums", votenums));
+		sb.append(makeTag("multiple", multiple));
+
+		sb.append(makeTag("voteinfo", voteinfo));
+
+		sb.append(makeTag("totalmoney", totalmoney));
+
+		sb.append(makeTag("playtype", playtype));
+
+		sb.append(makeTag("passtype", passtype));
+		sb.append(makeTag("buymoney", buymoney));
+		sb.append(makeTag("protype", protype));
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.2竞彩购买记录查询（12021_1.1）
+	 * 
+	 * @param uid
+	 * @param lotteryid
+	 * @param startdate
+	 * @param enddate
+	 * @param investtype
+	 * @param pageindex
+	 * @param pagesize
+	 * @param statue
+	 * @return
+	 */
+	public Request getPurchaseRecords(String uid, String lotteryid,
+			String startdate, String enddate, String investtype,
+			String pageindex, String pagesize, String statue) {
+
+		String body = createPurchaseRecords(uid, lotteryid, startdate, enddate,
+				investtype, pageindex, pagesize, statue);
+		String xmlBody = makeXml(body, "12021_1.1");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createPurchaseRecords(String uid, String lotteryid,
+			String startdate, String enddate, String investtype,
+			String pageindex, String pagesize, String statue) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("uid", uid));
+		sb.append(makeTag("lotteryid", lotteryid));
+		sb.append(makeTag("startdate", startdate));
+		sb.append(makeTag("enddate", enddate));
+		sb.append(makeTag("investtype", investtype));
+		sb.append(makeTag("pageindex", pageindex));
+		sb.append(makeTag("pagesize", pagesize));
+		sb.append(makeTag("statue", statue));
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.3竞彩购买记录详情查询（12022_1.1）
+	 * 
+	 * @param serialid
+	 * @return
+	 */
+	public Request getPurchaseRecordsDetail(String serialid) {
+
+		String body = createPurchaseRecordsDetail(serialid);
+		String xmlBody = makeXml(body, "12022_1.1");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createPurchaseRecordsDetail(String serialid) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("serialid", serialid));
 
 		sb.append("</element>");
 		sb.append("</elements>");
@@ -439,4 +557,439 @@ public class RequestMaker {
 		return sb.toString();
 	}
 
+	/**
+	 * 4.1.4竞彩推荐赛事历史查询（12025）
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public Request getGameHistorySearch(String date) {
+
+		String body = createGameHistorySearch(date);
+		String xmlBody = makeXml(body, "12025");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createGameHistorySearch(String date) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("date", date));
+
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.5竞彩历史推荐日期列表（12026）
+	 * 
+	 * @param startdate
+	 * @param enddate
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
+	public Request getGameHistoryDateList(String startdate, String enddate,
+			String pageNum, String pageSize) {
+
+		String body = createGameHistoryDateList(startdate, enddate, pageNum,
+				pageSize);
+		String xmlBody = makeXml(body, "12026");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createGameHistoryDateList(String startdate, String enddate,
+			String pageNum, String pageSize) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("startdate", startdate));
+		sb.append(makeTag("enddate", enddate));
+		sb.append(makeTag("pageNum", pageNum));
+		sb.append(makeTag("pageSize", pageSize));
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.6竞彩今天推荐赛事投票（12028）
+	 * 
+	 * @param uid
+	 * @param matchId
+	 * @param vote
+	 * @param content
+	 * @return
+	 */
+	public Request getGameTodayRecomend(String uid, String matchId,
+			String vote, String content) {
+
+		String body = createGameTodayRecomend(uid, matchId, vote, content);
+		String xmlBody = makeXml(body, "12028");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createGameTodayRecomend(String uid, String matchId,
+			String vote, String content) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("uid", uid));
+		sb.append(makeTag("matchId", matchId));
+		sb.append(makeTag("vote", vote));
+		sb.append(makeTag("content", content));
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.7竞彩中奖记录查询（12029）
+	 * 
+	 * @param uid
+	 * @param matchId
+	 * @param vote
+	 * @param content
+	 * @return
+	 */
+	public Request getGameWinRecord(String size) {
+
+		String body = createGameWinRecord(size);
+		String xmlBody = makeXml(body, "12029");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createGameWinRecord(String size) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("size", size));
+
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.8竞彩可投注赛事查询（12030）
+	 * 
+	 * @param pagenum
+	 * @param pagesize
+	 * @return
+	 */
+	public Request getGameCanBet(String pagenum, String pagesize) {
+
+		String body = createGameCanBet(pagenum, pagesize);
+		String xmlBody = makeXml(body, "12030");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createGameCanBet(String pagenum, String pagesize) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("pagenum", pagenum));
+		sb.append(makeTag("pagesize", pagesize));
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.9创建丰付支付订单(15006)
+	 * 
+	 * @param userIdIdentity
+	 * @param totalPrice
+	 * @return
+	 */
+	public Request getFengPay(String userIdIdentity, String totalPrice) {
+
+		String body = createFengPay(userIdIdentity, totalPrice);
+		String xmlBody = makeXml(body, "15006");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createFengPay(String userIdIdentity, String totalPrice) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("userIdIdentity", userIdIdentity));
+		sb.append(makeTag("totalPrice", totalPrice));
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.10丰付首次快捷支付（15007）
+	 * 
+	 * @param requestOrderId
+	 * @param bankCode
+	 * @param bankAccount
+	 * @param bankCardType
+	 * @param validDate
+	 * @param cvnCode
+	 * @param idType
+	 * @param idNumber
+	 * @param name
+	 * @param mobilePhone
+	 * @param isNeedBind
+	 * @param userIdIdentity
+	 * @param randomValidateId
+	 * @param randomCode
+	 * @param tradeId
+	 * @return
+	 */
+	public Request getFengPayFirst(String requestOrderId, String bankCode,
+			String bankAccount, String bankCardType, String validDate,
+			String cvnCode, String idType, String idNumber, String name,
+			String mobilePhone, String isNeedBind, String userIdIdentity,
+			String randomValidateId, String randomCode, String tradeId) {
+
+		String body = createFengPayFirst(requestOrderId, bankCode, bankAccount,
+				bankCardType, validDate, cvnCode, idType, idNumber, name,
+				mobilePhone, isNeedBind, userIdIdentity, randomValidateId,
+				randomCode, tradeId);
+		String xmlBody = makeXml(body, "15007");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createFengPayFirst(String requestOrderId, String bankCode,
+			String bankAccount, String bankCardType, String validDate,
+			String cvnCode, String idType, String idNumber, String name,
+			String mobilePhone, String isNeedBind, String userIdIdentity,
+			String randomValidateId, String randomCode, String tradeId) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("requestOrderId", requestOrderId));
+		sb.append(makeTag("bankCode", bankCode));
+		sb.append(makeTag("bankAccount", bankAccount));
+		sb.append(makeTag("bankCardType", bankCardType));
+		sb.append(makeTag("validDate", validDate));
+		sb.append(makeTag("cvnCode", cvnCode));
+		sb.append(makeTag("idType", idType));
+		sb.append(makeTag("idNumber", idNumber));
+		sb.append(makeTag("name", name));
+		sb.append(makeTag("mobilePhone", mobilePhone));
+		sb.append(makeTag("isNeedBind", isNeedBind));
+		sb.append(makeTag("userIdIdentity", userIdIdentity));
+		sb.append(makeTag("randomValidateId", randomValidateId));
+		sb.append(makeTag("randomCode", randomCode));
+		sb.append(makeTag("tradeId", tradeId));
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.11丰付非首次快捷支付（15008）
+	 */
+	public Request getFengPayNotFirst(String requestOrderId, String bankCode,
+			String bindId, String userIdIdentity, String randomValidateId,
+			String randomCode, String tradeId) {
+
+		String body = createFengPayNotFirst(requestOrderId, bankCode, bindId,
+				userIdIdentity, randomValidateId, randomCode, tradeId);
+		String xmlBody = makeXml(body, "15008");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createFengPayNotFirst(String requestOrderId,
+			String bankCode, String bindId, String userIdIdentity,
+			String randomValidateId, String randomCode, String tradeId) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("requestOrderId", requestOrderId));
+		sb.append(makeTag("bankCode", bankCode));
+		sb.append(makeTag("bindId", bindId));
+		sb.append(makeTag("userIdIdentity", userIdIdentity));
+		sb.append(makeTag("randomValidateId", randomValidateId));
+		sb.append(makeTag("randomCode", randomCode));
+		sb.append(makeTag("tradeId", tradeId));
+
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.12丰付首次快捷支付短信验证（15009）
+	 * 
+	 * @param requestOrderId
+	 * @param bankCode
+	 * @param bankAccount
+	 * @param validDate
+	 * @param bankCardType
+	 * @param cvnCode
+	 * @param idType
+	 * @param idNumber
+	 * @param name
+	 * @param mobilePhone
+	 * @param userIdIdentity
+	 * @return
+	 */
+	public Request getFengMessagePayFirst(String requestOrderId,
+			String bankCode, String bankAccount, String validDate,
+			String bankCardType, String cvnCode, String idType,
+			String idNumber, String name, String mobilePhone,
+			String userIdIdentity) {
+
+		String body = createFengMessagePayFirst(requestOrderId, bankCode,
+				bankAccount, validDate, bankCardType, cvnCode, idType,
+				idNumber, name, mobilePhone, userIdIdentity);
+		String xmlBody = makeXml(body, "15008");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createFengMessagePayFirst(String requestOrderId,
+			String bankCode, String bankAccount, String validDate,
+			String bankCardType, String cvnCode, String idType,
+			String idNumber, String name, String mobilePhone,
+			String userIdIdentity) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("requestOrderId", requestOrderId));
+		sb.append(makeTag("bankCode", bankCode));
+		sb.append(makeTag("bankAccount", bankAccount));
+		sb.append(makeTag("bankCardType", bankCardType));
+		sb.append(makeTag("validDate", validDate));
+
+		sb.append(makeTag("cvnCode", cvnCode));
+
+		sb.append(makeTag("idType", idType));
+		sb.append(makeTag("idNumber", idNumber));
+		sb.append(makeTag("name", name));
+		sb.append(makeTag("mobilePhone", mobilePhone));
+		sb.append(makeTag("userIdIdentity", userIdIdentity));
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
+
+	/**
+	 * 4.1.13丰付非首次快捷支付短信验证（15010）
+	 * 
+	 * @param requestOrderId
+	 * @param bankCode
+	 * @param bindId
+	 * @param userIdIdentity
+	 * @return
+	 */
+	public Request getFengMessagePayNotFirst(String requestOrderId,
+			String bankCode, String bindId, String userIdIdentity) {
+
+		String body = createFengMessagePayNotFirst(requestOrderId, bankCode,
+				bindId, userIdIdentity);
+		String xmlBody = makeXml(body, "15008");
+		LogUtil.log("xmlBody:" + xmlBody);
+
+		request.setBody(xmlBody);
+		request.setServerInterfaceDefinition(ServerInterfaceDefinition.OPT_GETCHANNELLIST);
+		request.setXmlParser(null);
+		return request;
+	}
+
+	private String createFengMessagePayNotFirst(String requestOrderId,
+			String bankCode, String bindId, String userIdIdentity) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("<body>");
+		sb.append("<elements>");
+		sb.append("<element>");
+		sb.append(makeTag("requestOrderId", requestOrderId));
+		sb.append(makeTag("bankCode", bankCode));
+		sb.append(makeTag("bindId", bindId));
+		sb.append(makeTag("userIdIdentity", userIdIdentity));
+
+		sb.append("</element>");
+		sb.append("</elements>");
+		sb.append("</body>");
+
+		return sb.toString();
+	}
 }
